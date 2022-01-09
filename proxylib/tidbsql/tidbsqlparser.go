bd2bbbd166768fec6ef1ff2c85a9c2f984e3e159
handlerww <lohipoo@yeah.net>
2022-01-09 14:57:29 +0800

    add error message

--- proxylib/tidbsql/tidbsqlparser.go
+++ proxylib/tidbsql/tidbsqlparser.go
@@ -164,7 +164,7 @@ func (p *parser) OnData(reply, endStream bool, dataArray [][]byte) (proxylib.OpT
 	}
 	if !matches {
 		// TODO: use MySQL ERROR packet
-		p.connection.Inject(true, constructErrorMessage("No database selected"))
+		p.connection.Inject(true, constructErrorMessage("No database selected", true))
 		log.Debugf("Policy mismatch, dropping %d bytes", msgLen)
 		return proxylib.DROP, msgLen
 	}
@@ -172,7 +172,7 @@ func (p *parser) OnData(reply, endStream bool, dataArray [][]byte) (proxylib.OpT
 	return proxylib.PASS, msgLen
 }
 
-func constructErrorMessage(errorMessage string) []byte {
+func constructErrorMessage(errorMessage string, dbErr bool) []byte {
 	// payloadLength is a fixed length integer: https://dev.mysql.com/doc/internals/en/integer.html#packet-Protocol::FixedLengthInteger
 	// nodatabaseSelectedErr = []byte{29 0 0 1 255 22 4 35 51 68 48 48 48 48 78 111 32 100 97 116 97 98 97 115 101 32 115 101 108 101 99 116 101 100}
 	// return nodatabaseSelectedErr
@@ -180,16 +180,23 @@ func constructErrorMessage(errorMessage string) []byte {
 	// https://dev.mysql.com/doc/internals/en/packet-ERR_Packet.html
 
 	sequenceID := 1
-	header := []byte{0xff}
-	errorCode := []byte{0x16, 0x04} // 1046?
-	sqlStateMarker := []byte{0x23}
-	sqlState := []byte("3D000")
+	header := []byte{0xff} // for error packet, it's 0xff
+	var errCode []byte
+	// handle db err and table err
+	if dbErr {
+		errCode = []byte{0x14, 0x04} // 1044, 42000 ER_DBACCESS_DENIED_ERROR
+	} else {
+		errCode = []byte{0x12, 0x04} // 1042, 42000 ER_TABLEACCESS_DENIED_ERROR
+	}
+	sqlStateMarker := []byte{0x23} // TODO: why this value
+
+	sqlState := []byte{0x10, 0xa4} // 42000
 	payload := []byte{}
 	payload = append(payload, header...)
-	payload = append(payload, errorCode...)
+	payload = append(payload, errCode...)
 	payload = append(payload, sqlStateMarker...)
 	payload = append(payload, sqlState...)
 	payload = append(payload, []byte(errorMessage)...)
 	payloadLength := len(payload)
-	return append([]byte{byte(payloadLength), 0, 0, byte(sequenceID)}, payload...)
+	return append([]byte{byte(payloadLength), 0x00, 0x00, byte(sequenceID)}, payload...)
 }
